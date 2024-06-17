7d9b39478c2e465319acb0dec87b3e456f7c7eff
Marco <marko.barraza@gmail.com>
2024-06-16 20:12:46 -0400

    se agrego ejercicio 4-colores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 let ele = document.getElementById("ele1")
+ele.style.backgroundColor = "green"  
 
 // Funcion pintar fondo texto
 function pintar(color = "green"){
     ele.style.backgroundColor = color
-}
-
-ele.style.backgroundColor = "green"
-    
+}  
     
 // al hacer click se activa la funcion
 ele.addEventListener("click", function(){
     pintar("yellow")
 });
+
+
+
+
