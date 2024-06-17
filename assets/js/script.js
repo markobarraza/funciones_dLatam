6b5388d20f980f674daaeaf0a121a30f40b1c9bc
Marco <marko.barraza@gmail.com>
2024-06-16 20:13:37 -0400

    archivos modificados

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,29 @@ ele.addEventListener("click", function(){
 
 
 
+
+const caja_negra = (a) =>{
+    let prueba = document.querySelector("#box-" + a)
+    prueba.style.backgroundColor = "black"
+}
+
+let caja_azul = document.getElementById("box-azul")
+let caja_rojo = document.getElementById("box-rojo")
+let caja_verde = document.getElementById("box-verde")
+let caja_amarillo = document.getElementById("box-amarillo")
+
+caja_azul.addEventListener("click", function(){
+    caja_negra("azul")
+})
+caja_rojo.addEventListener("click", function(){
+    caja_negra("rojo")
+})
+caja_verde.addEventListener("click", function(){
+    caja_negra("verde")
+})
+caja_amarillo.addEventListener("click", function(){
+    caja_negra("amarillo")
+})
+
+
+
